77d11078d5018b920ceddd5b30711fc078c3b627
Rajnish Kumar Pandey <rajnish2003kumar@gmail.com>
2026-02-05 18:38:13 +0530

    Remove unused imports (fix Vercel build)

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { SiNextdotjs, SiSolidity } from "react-icons/si";
-import { FaRust } from "react-icons/fa";
 import Go from "../../Assets/TechIcons/go.svg";
 import C from "../../Assets/TechIcons/C++.svg";
 import Javascript from "../../Assets/TechIcons/Javascript.svg";
